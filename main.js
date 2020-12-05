51507988ac4e0140403e48515914d0972179125c
Leon0241 <enliang.leon.lee@gmail.com>
2020-12-05 21:52:06 +0000

    essentials of design

--- main.js
+++ main.js
@@ -4,14 +4,18 @@ const gameStatsArea = document.createElement('div')
 const gameWordArea = document.createElement('div')
 const gameTypingArea = document.createElement('div')
 const gameResetButton = document.createElement('div')
+const gameTypingField = document.createElement('input')
 gameStatsArea.classList.add("gameStatsArea")
 gameWordArea.classList.add("gameWordArea")
 gameTypingArea.classList.add("gameTypingArea")
 gameResetButton.classList.add("gameResetButton")
+gameTypingField.classList.add("gameTypingField")
+gameTypingField.setAttribute("type", "text")
 
 window.onload = () => {
   gameContainer.appendChild(gameStatsArea)
   gameContainer.appendChild(gameWordArea)
   gameContainer.appendChild(gameTypingArea)
   gameContainer.appendChild(gameResetButton)
+  gameTypingArea.appendChild(gameTypingField)
 }
