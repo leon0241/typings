cd1dc4eacbb4f3934b267844f214331168144aa7
Leon0241 <enliang.leon.lee@gmail.com>
2021-02-26 23:34:40 +0000

    doing some styling

--- main.ts
+++ main.ts
@@ -1,29 +1,35 @@
-const gameContainer = document.querySelector('.gameContainer')
+const gameContainer = document.querySelector(".gameContainer")
 
-const gameStatsArea = document.createElement('div')
-const gameWordArea = document.createElement('div')
-const gameTypingArea = document.createElement('div')
-const gameResetButton = document.createElement('div')
-const gameTypingField = document.createElement('input')
-const gameProgress = document.createElement('span')
-const gameWPM = document.createElement('span')
-const gameAccuracy = document.createElement('span')
+const gameStatsArea = document.createElement("div")
 gameStatsArea.classList.add("gameStatsArea")
+const gameWordArea = document.createElement("div")
 gameWordArea.classList.add("gameWordArea")
+const gameTypingArea = document.createElement("div")
 gameTypingArea.classList.add("gameTypingArea")
+const gameResetButton = document.createElement("div")
 gameResetButton.classList.add("gameResetButton")
+
+const gameTypingField = document.createElement("input")
 gameTypingField.classList.add("gameTypingField")
 gameTypingField.setAttribute("type", "text")
+
+const gameProgress = document.createElement("span")
 gameProgress.classList.add("gameProgress")
+const gameWPM = document.createElement("span")
 gameWPM.classList.add("gameWPM")
+const gameAccuracy = document.createElement("span")
 gameAccuracy.classList.add("gameAccuracy")
+
+const startOverlay = document.createElement("div")
 
 window.onload = () => {
   gameContainer.appendChild(gameStatsArea)
   gameContainer.appendChild(gameWordArea)
   gameContainer.appendChild(gameTypingArea)
   gameContainer.appendChild(gameResetButton)
+
   gameTypingArea.appendChild(gameTypingField)
+
   gameStatsArea.appendChild(gameProgress)
   gameStatsArea.appendChild(gameWPM)
   gameStatsArea.appendChild(gameAccuracy)
