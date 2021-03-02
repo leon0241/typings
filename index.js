9c3b2c6130b43bf2cb832651d5c5bc508771e07b
Leon0241 <enliang.leon.lee@gmail.com>
2021-03-02 03:14:17 +0000

    how does mongoose work

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose')
+
+const mongo = "mongodb+srv://leon024:9zRcm0xw5wOcipba@leaderboard.eejxy.mongodb.net/Leaderboard?retryWrites=true&w=majority"
+
+// let Schema = mongoose.Schema;
+
+// let SomeModelSchema = new Schema({
+//   name: String,
+//   wpm: Number,
+//   acc: Number
+// });
+
+mongoose.connect(mongo)
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-   res.send('Hello World!')
- })
+   res.sendFile('index.html');
+});
  
  app.listen(port, () => {
    console.log(`Example app listening at http://localhost:${port}`)
